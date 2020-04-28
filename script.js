13a58ef80ec0222cb8e9a99e20fd4c672150e3e1
usman-203 <62180188+usman-203@users.noreply.github.com>
2020-04-28 14:12:17 -0700

    commit5

--- script.js
+++ script.js
@@ -34,3 +34,35 @@ function showMillionaires() {
     data = data.filter(user => user.money > 1000000);
     updateDOM();
   }
+  function calculateWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+  
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+      wealth
+    )}</strong></h3>`;
+    main.appendChild(wealthEl);
+  }
+function addData(obj) {
+  data.push(obj);
+  updateDOM();
+}
+function updateDOM(providedData = data) {
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+  providedData.forEach(item => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+addUserButton.addEventListener('click', randomuser);
+doubleMoneyButton.addEventListener('click', doubleMoney);
+sortButton.addEventListener('click', sortByRichest);
+showMillionairesButton.addEventListener('click', showMillionaires);
+calculateWealthButton.addEventListener('click', calculateWealth);
